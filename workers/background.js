ff2e8ffaabaa71e8b1144e26661bae84d7c9cc1d
Antoine Auger <antoine.auger@epita.fr>
2023-03-30 14:29:29 +0200

    [FIX] handleChanged now gets the file path

--- workers/background.js
+++ workers/background.js
@@ -55,15 +55,20 @@ chrome.downloads.onCreated.addListener(
   )
 
 // Start when a download is changed
-// Need to check if the download is complete and if it is a torrent file
 chrome.downloads.onChanged.addListener(handleChanged)
 
+// Get the path of the dl file if it's a torrent file
+// Will launch the function that will encrypt the file in base64
 function handleChanged(delta) {
     if (delta.state && delta.state.current === "complete") {
-        //if(isTorrentFile(delta.filename))
-        //{
-        //    console.log("Torrent file")
-        //}
+        chrome.downloads.search({id: delta.id}, function(items) {
+            if(isTorrentFile(items[0].filename))
+            {
+                console.log("Torrent file")
+            }
+            console.log(items[0].filename)
+        });
+        
         console.log(`Download ${delta.id} has completed.`);
     }
 }
