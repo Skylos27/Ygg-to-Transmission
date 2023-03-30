335c5937ad9d27b14e357fa6e87839b774d512a7
Antoine Auger <antoine.auger@epita.fr>
2023-03-30 12:27:08 +0200

    Background worker enable

---
+++ workers/background.js
@@ -0,0 +1,69 @@
+const icons = {
+    enabled: "images/icon.png",
+    disabled: "images/disabled-icon.png"
+}
+console.log("Background script started")
+
+// Function that get the path of a file downloaded on Google Chrome 
+
+function isTorrentFile(path) {
+    return path.endsWith(".torrent")
+}
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete") {
+        const urlValid = tab?.url.includes("yggtorrent")
+        if(urlValid)
+        {
+            console.log("Valid1")
+        }
+        else
+        {
+            console.log("Not valid1")
+        }
+    }
+
+    chrome.storage.sync.get(["ready", "timestamp"], ({ready, timestamp}) => {
+        if (ready && Date.now() - timestamp > maxRequestAge)
+            chrome.storage.sync.set({ready: false})
+
+    });
+})
+
+chrome.tabs.onActivated.addListener(({tabId}) => {
+
+    chrome.tabs.get(tabId, (tab) => {
+        const urlValid = tab?.url.includes("yggtorrent")
+        if(urlValid)
+        {
+            console.log("Valid2")
+        }
+        else
+        {
+            console.log("Not valid2")
+        }
+    })
+
+});
+
+// Start when a download is created
+chrome.downloads.onCreated.addListener(
+    function(downloadItem) {
+        console.log(downloadItem.state)
+    }
+  )
+
+// Start when a download is changed
+// Need to check if the download is complete and if it is a torrent file
+chrome.downloads.onChanged.addListener(
+    function(downloadItem) {
+        if(downloadItem.state == "complete")
+        {
+            if(isTorrentFile(downloadItem.filename))
+            {
+                console.log("Torrent file downloaded")
+            }
+        }
+    }
+       
+)
