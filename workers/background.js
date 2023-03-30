9334cc644f28e6791fe8d1c57df6e088b0d6ce96
Antoine Auger <antoine.auger@epita.fr>
2023-03-30 15:56:11 +0200

    [ADD] handlechanged only activate on ygg tab

--- workers/background.js
+++ workers/background.js
@@ -1,25 +1,17 @@
 const icons = {
-    enabled: "images/icon.png",
-    disabled: "images/disabled-icon.png"
+    enabled: "../images/icon.png",
+    disabled: "../images/disabled-icon.png"
 }
 console.log("Background script started")
 
 // Function that get the path of a file downloaded on Google Chrome 
-
+urlValid = false
 function isTorrentFile(path) {
     return path.endsWith(".torrent")
 }
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete") {
-        const urlValid = tab?.url.includes("yggtorrent")
-        if(urlValid)
-        {
-            console.log("Valid1")
-        }
-        else
-        {
-            console.log("Not valid1")
-        }
+        urlValid = tab?.url.includes("yggtorrent")
     }
 
     chrome.storage.sync.get(["ready", "timestamp"], ({ready, timestamp}) => {
@@ -32,27 +24,10 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 chrome.tabs.onActivated.addListener(({tabId}) => {
 
     chrome.tabs.get(tabId, (tab) => {
-        const urlValid = tab?.url.includes("yggtorrent")
-        if(urlValid)
-        {
-            console.log("Valid2")
-        }
-        else
-        {
-            console.log("Not valid2")
-        }
+        urlValid = tab?.url.includes("yggtorrent")
     })
 
 });
-
-// Start when a download is created
-chrome.downloads.onCreated.addListener(
-    function(downloadItem) {
-        downloadId = downloadItem.id
-        path = downloadItem.filename
-        console.log(downloadItem.state)
-    }
-  )
 
 // Start when a download is changed
 chrome.downloads.onChanged.addListener(handleChanged)
@@ -60,7 +35,7 @@ chrome.downloads.onChanged.addListener(handleChanged)
 // Get the path of the dl file if it's a torrent file
 // Will launch the function that will encrypt the file in base64
 function handleChanged(delta) {
-    if (delta.state && delta.state.current === "complete") {
+    if (delta.state && delta.state.current === "complete" && urlValid) {
         chrome.downloads.search({id: delta.id}, function(items) {
             if(isTorrentFile(items[0].filename))
             {
